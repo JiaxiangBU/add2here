adfcc0c13812ea3d19d9454c3400f1881fc3af67
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2020-02-02 17:47:10 +0800

    fix rstudio not run bugs.

--- R/add2here.R
+++ R/add2here.R
@@ -1,7 +1,8 @@
 add2here <- function(html_path = "output/git-github-gitlab-learning-notes.html",
                      path_to_root = "..") {
     html_content <- readr::read_lines(html_path)
-    local_project_path <- rstudioapi::getActiveProject()
+    local_project_path <- usethis::proj_path()
+    # usethis::proj_path() 不会报错
     html_content %>%
         stringr::str_replace_all(local_project_path, path_to_root) %>%
         readr::write_lines(html_path)
